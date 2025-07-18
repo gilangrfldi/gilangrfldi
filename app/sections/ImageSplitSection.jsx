5e75afdcb043802a697acc79ad567527b415de7c
gilangrfldi <123294925+gilangrfldi@users.noreply.github.com>
2025-07-18 23:00:57 +0700

    testing 2 ImageSplitSection and Tailwind3

--- app/sections/ImageSplitSection.jsx
+++ app/sections/ImageSplitSection.jsx
@@ -1,76 +1,45 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import RandomQuote from "../components/RandomQuote/RandomQuote";
+
 gsap.registerPlugin(ScrollTrigger);
+export default function ImageSplitSection() {
+    const sectionRef = useRef(null);
 
-export default function ImageSplitSection({ innerRef }) {
     useEffect(() => {
-        const sectionElement = innerRef.current;
-        if (!sectionElement) return;
-
-        ScrollTrigger.getAll().forEach((trigger) => {
-            if (trigger.trigger === sectionElement) {
-                trigger.kill();
-            }
-        });
-
-        gsap.fromTo(
-            sectionElement,
-            { opacity: 1, scale: 1 },
-            {
-                opacity: 1,
-                scale: 1,
-                ease: "power2.inOut",
+        const sectionElement = sectionRef.current;
+        let ctx = gsap.context(() => {
+            const timeline = gsap.timeline({
                 scrollTrigger: {
                     trigger: sectionElement,
                     start: "center center",
-                    end: "top+=50%",
+                    end: "+=600",
+                    pin: true,
                     scrub: 1,
+                    anticipatePin: 1,
                 },
-            }
-        );
-
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: sectionElement,
-                start: "center center",
-                end: "+=600",
-                pin: true,
-                scrub: 1,
-                anticipatePin: 1,
-            },
-        });
-
-        timeline.to({}, { duration: 0.2 });
-
-        timeline.to(`${sectionElement.className} .image-top`, {
-            yPercent: -100,
-            ease: "power2.inOut",
-        });
-        timeline.to(`${sectionElement.className} .image-bottom`, { yPercent: 100, ease: "power2.inOut" }, "<");
-
-        timeline.fromTo(`${sectionElement.className} .first-text`, { opacity: 0 }, { opacity: 1, y: "-22vh", ease: "power1.in" }, "<+=0.12");
-
-        timeline.fromTo(
-            `${sectionElement.className} .second-content`,
-            { opacity: 0 },
-            { opacity: 1, y: "-15vh", ease: "power2.out", stagger: 0.2 },
-            ">-0.20"
-        );
-
-        return () => {
-            ScrollTrigger.getAll().forEach((trigger) => {
-                if (trigger.trigger === sectionElement) {
-                    trigger.kill();
-                }
             });
-        };
-    }, [innerRef]);
+
+            timeline.to({}, { duration: 0.2 });
+
+            timeline.to(".image-top", {
+                yPercent: -100,
+                ease: "power2.inOut",
+            });
+            timeline.to(".image-bottom", { yPercent: 100, ease: "power2.inOut" }, "<");
+
+            timeline.fromTo(".first-text", { opacity: 0 }, { opacity: 1, y: "-22vh", ease: "power1.in" }, "<+=0.12");
+
+            timeline.fromTo(".second-content", { opacity: 0 }, { opacity: 1, y: "-15vh", ease: "power2.out", stagger: 0.2 }, ">-0.20");
+        }, sectionRef);
+
+        return () => ctx.revert();
+    }, []); 
 
     return (
-        <div className='ImageSplitSection relative h-screen w-full overflow-hidden bg-[#2c2c34] '>
+        <div ref={sectionRef} className='ImageSplitSection relative h-screen w-full overflow-hidden bg-[#2c2c34]'>
             <div className='image-top absolute top-0 left-0 h-1/2 w-full overflow-hidden'>
                 <div className='image-inner absolute top-0 h-screen w-full'></div>
             </div>
