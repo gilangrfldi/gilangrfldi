ccf587ac1e3830c1e3eec3c01f320ace355733b9
gilangrfldi <123294925+gilangrfldi@users.noreply.github.com>
2025-07-18 22:27:12 +0700

    delete usestate

--- app/sections/ImageSplitSection.jsx
+++ app/sections/ImageSplitSection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import RandomQuote from "../components/RandomQuote/RandomQuote";
